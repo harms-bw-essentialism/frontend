cb9b2730ed0195aa907e6b0bc90933ecaea99744
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-27 22:44:54 -0700

    container styling

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,21 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Routes from "./Routes";
 import { Container, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import NavBar from "../Common/components/NavBar";
 
 const App = () => {
+  const useStyles = makeStyles({
+    appContainer: {
+      height: "100vh",
+      width: "100vw"
+    }
+  });
+
+  const classes = useStyles();
+
   return (
-    <Container>
+    <Container className={classes.appContainer}>
       <Box my={2}>
         <NavBar />
         <React.Fragment>
