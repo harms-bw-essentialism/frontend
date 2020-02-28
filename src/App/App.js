14a2c7c009d8bea9eec8e67fd1a15da1ba2d7df7
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-27 19:23:59 -0700

    refactored to functional component

--- src/App/App.js
+++ src/App/App.js
@@ -1,31 +1,26 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Routes from "./Routes";
+import { Container, Box } from "@material-ui/core";
+import NavBar from "../Common/components/NavBar";
 
-import { connect } from "react-redux";
-
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Switch>
-          {Routes.map(route => (
-            <Route exact path={route.path} key={route.path}>
-              <route.component classes={{}} />
-            </Route>
-          ))}
-        </Switch>
-      </React.Fragment>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {};
+const App = () => {
+  return (
+    <Container>
+      <Box my={2}>
+        <NavBar />
+        <React.Fragment>
+          <Switch>
+            {Routes.map(route => (
+              <Route exact path={route.path} key={route.path}>
+                <route.component classes={{}} />
+              </Route>
+            ))}
+          </Switch>
+        </React.Fragment>
+      </Box>
+    </Container>
+  );
 };
 
-const mapDispatchToProps = dispatch => {
-  return {};
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
