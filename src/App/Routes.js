586fd9c8824f360d3097b935445da2e7780c3af5
Sarah Flowers <flowersfaerie@gmail.com>
2020-02-29 09:30:24 -0800

    Completed designing the login and register forms.

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -1,9 +1,15 @@
 import LoginForm from "../Features/onboarding/LoginForm";
+import RegisterForm from "../Features/onboarding/RegisterForm";
 
 const Routes = [
   {
     path: "/login",
     component: LoginForm
+  },
+  
+  {
+    path: "/register",
+    component: RegisterForm
   }
 ];
 
