91f13364d9cef27ac9ecdcc453f260ea4f71db44
Cody Brewer <brewer.cody.m@gmail.com>
2020-03-03 13:42:11 -0700

    renamed "creators" to actions.js

--- src/Redux/Actions/creators.js
+++
@@ -1,9 +0,0 @@
-import { SELECT_VALUE, NARROW_VALUE } from "./types";
-
-export const selectValue = item => dispatch => {
-  dispatch({ type: SELECT_VALUE, payload: item });
-};
-
-export const narrowValue = item => dispatch => {
-  dispatch({ type: NARROW_VALUE, payload: item });
-};
