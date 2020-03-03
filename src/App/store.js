0ed6ac8934702a280d1a53a27c5d6dc2d8e0b505
Cody Brewer <brewer.cody.m@gmail.com>
2020-03-03 11:14:26 -0700

    add persisted state

--- src/App/store.js
+++ src/App/store.js
@@ -1,14 +1,29 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { loadState, saveState } from "../Common/Utils/localStorage";
 import rootReducer from "./rootReducer";
 import thunk from "redux-thunk";
+import throttle from "lodash/throttle";
 
 // Enable Redux Dev Tools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Create Store
+const persisitedState = loadState();
 const store = createStore(
   rootReducer,
+  persisitedState,
   composeEnhancers(applyMiddleware(thunk))
+);
+
+store.subscribe(
+  throttle(() => {
+    saveState(
+      store.getState({
+        values: store.getState().values,
+        token: store.getState().user.token
+      })
+    );
+  }, 1000)
 );
 
 export default store;
