f79e88bf32a1b95e8f2dad7f27ea097a08226402
Cody Brewer <brewer.cody.m@gmail.com>
2020-03-05 23:41:45 -0700

    commented out onSelect method is not defined

--- src/Features/dashboard/Projects.js
+++ src/Features/dashboard/Projects.js
@@ -1,52 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    Typography,
-    List,
-    ListItem,
-    ListItemText,
-    ListItemSecondaryAction,
-    IconButton
-} from '@material-ui/core';
-import {Paper} from '@material-ui/core'
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton
+} from "@material-ui/core";
+import { Paper } from "@material-ui/core";
 // import { withStyles } from '@material-ui/core/styles';
 // import { Edit, Delete } from '@material-ui/icons';
-import axios from 'axios';
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const styles = {};
 
 const Projects = () => {
-    const [project, setProject] = useState();
+  const projects = useSelector(state => state.projects.projects);
 
-    useEffect(() => {
-        axios
-            .get('https://essentialism2020.herokuapp.com/api/essentialism/projects/user/:id')
-            .then(response => setProject(response.data.results))
-            .catch(err => console.log('Error', err));
-    }, []);
-
-    return (
+  return (
     <React.Fragment>
-        <List component='ul'>
-            {project.map(({projectId, projectName}) =>(
-                <ListItem
-                    key={projectId}
-                    button
-                    onClick={() => onSelect(projectId)}
-                >
-                    <ListItemText primary={projectName} />
-                    <ListItemSecondAction>
-                        <IconButton
-                            color='primary'
-                            onClick={() => ondeviceorientationabsolute(projectId)}
-                        >
-                            <Delete />
-                        </IconButton>
-                    </ListItemSecondAction>
-                </ListItem>
-            ))}
-        </List>
+      <List component="ul">
+        {projects.map(({ projectId, projectName }) => (
+          //   <ListItem key={projectId} button onClick={() => onSelect(projectId)}>
+          <ListItem key={projectId} button onClick={() => {}}>
+            <ListItemText primary={projectName} />
+            <ListItemSecondAction>
+              <IconButton
+                color="primary"
+                onClick={() => ondeviceorientationabsolute(projectId)}
+              >
+                <Delete />
+              </IconButton>
+            </ListItemSecondAction>
+          </ListItem>
+        ))}
+      </List>
     </React.Fragment>
-    )
+  );
 };
 
 export default Projects;
