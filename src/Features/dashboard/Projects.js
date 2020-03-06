aaf8e549e844f5a844cba658c5563351f5fac85f
Sarah Flowers <flowersfaerie@gmail.com>
2020-03-05 22:05:35 -0800

    Created the basic dashboard files for both Values and Projects.

--- src/Features/dashboard/Projects.js
+++ src/Features/dashboard/Projects.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+    Typography,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+    IconButton
+} from '@material-ui/core';
+import {Paper} from '@material-ui/core'
+// import { withStyles } from '@material-ui/core/styles';
+// import { Edit, Delete } from '@material-ui/icons';
+import axios from 'axios';
+
+const styles = {};
 
 const Projects = () => {
-    return
-        <div></div>
+    const [project, setProject] = useState();
+
+    useEffect(() => {
+        axios
+            .get('https://essentialism2020.herokuapp.com/api/essentialism/projects/user/:id')
+            .then(response => setProject(response.data.results))
+            .catch(err => console.log('Error', err));
+    }, []);
+
+    return (
+    <React.Fragment>
+        <List component='ul'>
+            {project.map(({projectId, projectName}) =>(
+                <ListItem
+                    key={projectId}
+                    button
+                    onClick={() => onSelect(projectId)}
+                >
+                    <ListItemText primary={projectName} />
+                    <ListItemSecondAction>
+                        <IconButton
+                            color='primary'
+                            onClick={() => ondeviceorientationabsolute(projectId)}
+                        >
+                            <Delete />
+                        </IconButton>
+                    </ListItemSecondAction>
+                </ListItem>
+            ))}
+        </List>
+    </React.Fragment>
+    )
 };
 
-export default Values;
+export default Projects;
--- src/Features/dashboard/Values.js
+++ src/Features/dashboard/Values.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+    Typography,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+    IconButton
+} from '@material-ui/core';
+import {Paper} from '@material-ui/core'
+// import { withStyles } from '@material-ui/core/styles';
+// import { Edit, Delete } from '@material-ui/icons';
+import axios from 'axios';
+import Projects from './Projects';
+
+const styles = {};
 
 const Values = () => {
-    return
-        <div></div>
+    const [values, setValues] = useState();
+
+    useEffect(() => {
+        axios
+            .get('https://essentialism2020.herokuapp.com/api/essentialism/values/user/:id')
+            .then(response => setValues(response.data.results))
+            .catch(err => console.log('Error', err));
+    }, []);
+
+    return (
+    <React.Fragment>
+        <Paper elevation={3}>
+            values.map(value => (
+                <Fragment key={value.valueId}>
+                    <Typography
+                        className={classes.valueName}
+                        color='secondary'
+                        variant='h5'
+                    >
+                        {value.valueName}
+                    </Typography>
+                    <Projects />
+                </Fragment>
+            ))
+                        
+        </Paper>
+    </React.Fragment>
+    )
 };
 
 export default Values;
