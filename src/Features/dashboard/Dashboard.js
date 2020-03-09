c5b6d085b3260011297be7dbde96a04cdfca485a
Cody Brewer <brewer.cody.m@gmail.com>
2020-03-09 15:58:09 -0600

    add reselect values button to dashboard

--- src/Features/dashboard/Dashboard.js
+++ src/Features/dashboard/Dashboard.js
@@ -3,8 +3,11 @@ import Projects from "./Projects";
 import Values from "./Values";
 import { Divider } from "@material-ui/core";
 import Submission from "../InputProjects/Submission";
+import { useHistory } from "react-router-dom";
 
 const Dashboard = () => {
+  const history = useHistory();
+
   return (
     <>
       <h2>Projects</h2>
@@ -12,6 +15,14 @@ const Dashboard = () => {
       <Divider />
       <h2>Values</h2>
       <Values />
+      <Button
+        onClick={evt => {
+          evt.preventDefault();
+          history.push("/processing");
+        }}
+      >
+        Reselect Values
+      </Button>
     </>
   );
 };
